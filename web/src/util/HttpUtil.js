bbbba276955c54d0afdfcbea9da1d5de542c30fc
yjyvsdream <jingyuan.yu@myxxjs.com>
2018-10-12 16:34:31 +0800

    修改：服务器异常时，统一报消息

--- web/src/util/HttpUtil.js
+++ web/src/util/HttpUtil.js
@@ -1,4 +1,3 @@
-
 const httpHeaders = require( './HttpHeaders');
 
 const simpleFetch = (method,url,params) =>{
@@ -6,32 +5,38 @@ const simpleFetch = (method,url,params) =>{
     let options  ={
         method: method,
         headers: httpHeaders.getHeaders()
-    }
+    };
     if('GET' != method){
         options.body = JSON.stringify(params)
     }
     return fetch('http://'+url, options).then((response) => {
         $("#preload").hide();
-        let json = response.json();
-        return json;
+
+        // http 正常处理，则返回response，否则进入catch 处理
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw new Error();
+        }
     }).catch(function (error) {
         $("#preload").hide();
+        swal('服务器内部错误!', '', 'error');
         return error;
     });
-}
+};
 
 export const httpGet = (url)=> {
     return simpleFetch('GET',url,{});
-}
+};
 export const httpPost = (url, params) => {
     return simpleFetch('POST',url,params)
-}
+};
 export const httpPut = (url, params) => {
     return simpleFetch('PUT',url,params)
-}
+};
 export const httpDelete = (url, params) => {
     return simpleFetch('DELETE',url,params)
-}
+};
 
 export const httpAsyncGet = (url,callback) => {
     fetch('http://'+url, {
@@ -46,7 +51,7 @@ export const httpAsyncGet = (url,callback) => {
     }).catch((error)=>{
         callback(error,null);
     })
-}
+};
 
 export const httpAsyncPost = (url, params,callback) => {
     //console.log(httpHeaders.headers);
@@ -62,5 +67,5 @@ export const httpAsyncPost = (url, params,callback) => {
     }).catch((error)=>{
         callback(error,null);
     })
-}
+};
 
