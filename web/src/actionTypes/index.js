77bfcc174f889bc38fb74d437c77e08301e27b94
yjyvsdream <jingyuan.yu@myxxjs.com>
2018-10-15 17:03:01 +0800

    新建：交警管理 - 交警资料 画面显示 部分做成

--- web/src/actionTypes/index.js
+++ web/src/actionTypes/index.js
@@ -1,9 +1,11 @@
 import * as HeaderActionType  from './layout/HeaderActionType';
 import * as LoginActionType  from './layout/LoginActionType';
 import * as TrafficPoliceActionType  from './main/TrafficPoliceActionType';
+import * as TrafficPoliceDetailActionType  from './main/TrafficPoliceDetailActionType';
 
 export  {
     LoginActionType,
     HeaderActionType,
-    TrafficPoliceActionType
+    TrafficPoliceActionType,
+    TrafficPoliceDetailActionType
 }
--- web/src/components/main/TrafficPoliceDetail.jsx
+++ web/src/components/main/TrafficPoliceDetail.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import Select from 'react-select';
-import {TrafficPoliceActionType} from '../../actionTypes';
+import {TrafficPoliceDetailActionType} from '../../actionTypes';
 import {  Link } from "react-router-dom";
 
 const trafficPoliceAction = require('../../actions/main/TrafficPoliceAction');
@@ -49,103 +49,73 @@ class TrafficPolice extends React.Component {
     };
 
     render() {
-        const {trafficPoliceReducer, changeConditionGender, changeConditionPosition, changeConditionStatus} = this.props;
+        const {trafficPoliceReducer, changePolicePosition, changeStatus, updatePolice} = this.props;
         return (
             <div>
                 {/* 标题部分 */}
                 <div className="row">
                     <div className="input-field col s12 page-title">
-                        交警管理
+                        交警管理 - 交警资料
                         <div className="divider custom-divider margin-top10"/>
                     </div>
                 </div>
 
-                {/* 上部分：检索条件输入区域 */}
-                <div className="row grey-text text-darken-1">
-                    <div className="col s10 no-padding">
+                {/* 交警资料：明细 */}
+                <div className="row z-depth-1 detail-box margin-top40 margin-left50 margin-right50">
+                    <div className="row detail-box-header vc-center">
+                        {/* 交警资料：编号 */}
+                        <div className="col s6 context-ellipsis">编号：ssssssss</div>
 
-                        {/* 查询条件：第一行 */}
-                        <div>
-                            {/* 查询条件：编号 */}
-                            <div className="input-field col s4">
-                                <input id="police_no" type="text" value={trafficPoliceReducer.conditionNo} onChange={this.changeConditionNo}/>
-                                <label htmlFor="police_no">编号</label>
-                            </div>
-
-                            {/* 查询条件：姓名 */}
-                            <div className="input-field col s4">
-                                <input id="name" type="text" value={trafficPoliceReducer.conditionName} onChange={this.changeConditionName}/>
-                                <label htmlFor="name">姓名</label>
-                            </div>
-
-                            {/* 查询条件：性别 */}
-                            <div className="input-field col s4">
-                                <Select
-                                    options={trafficPoliceReducer.genderList}
-                                    onChange={changeConditionGender}
-                                    isSearchable={false}
-                                    placeholder={"性别"}
-                                    styles={SysConst.CUSTOM_REACT_SELECT_STYLE}
-                                    isClearable={false}
-                                />
-                            </div>
-
+                        {/* 交警资料：开关 */}
+                        <div className="col s6 right-align">
+                        <span className="switch" onClick={changeStatus}>
+                            <label>
+                              <input type="checkbox"/>
+                              <span className="lever"/>
+                            </label>
+                        </span>
                         </div>
-
-                        {/* 查询条件：第二行 */}
-                        <div>
-                            {/* 查询条件：职务 */}
-                            <div className="input-field col s4">
-                                <Select
-                                    options={trafficPoliceReducer.policePositionList}
-                                    onChange={changeConditionPosition}
-                                    isSearchable={false}
-                                    placeholder={"职务"}
-                                    styles={SysConst.CUSTOM_REACT_SELECT_STYLE}
-                                    isClearable={false}
-                                />
-                            </div>
-
-                            {/* 查询条件：电话 */}
-                            <div className="input-field col s4">
-                                <input id="phone" type="text" value={trafficPoliceReducer.conditionPhone} onChange={this.changeConditionPhone}/>
-                                <label htmlFor="phone">电话</label>
-                            </div>
-
-                            {/* 查询条件：状态 */}
-                            <div className="input-field col s4">
-                                <Select
-                                    options={trafficPoliceReducer.policeStatusList}
-                                    onChange={changeConditionStatus}
-                                    isSearchable={false}
-                                    placeholder={"状态"}
-                                    styles={SysConst.CUSTOM_REACT_SELECT_STYLE}
-                                    isClearable={false}
-                                />
-                            </div>
+                    </div>
+                    <div className="row margin-left10 margin-right10">
+                        <div className="input-field col s4">
+                            <input id="name" type="text" maxLength="100" value={trafficPoliceReducer.name} onChange={this.changeName}/>
+                            <label htmlFor="name">姓名</label>
                         </div>
+                        <div className="col s2 margin-top25">
+                            <input type="radio" id="male"   value="0" className='with-gap' checked={trafficPoliceReducer.gender==='0'} onChange={this.changeGender}/>
+                            <label htmlFor="male">男</label>
 
+                            <input type="radio" id="female" value="1" className='with-gap' checked={trafficPoliceReducer.gender==='1'} onChange={this.changeGender}/>
+                            <label htmlFor="female" className="margin-left10">女</label>
+                        </div>
+                        <div className="input-field col s6">
+                            <Select
+                                options={trafficPoliceReducer.policePositionList}
+                                onChange={changePolicePosition}
+                                isSearchable={false}
+                                value={trafficPoliceReducer.position}
+                                placeholder={"职务"}
+                                styles={SysConst.CUSTOM_REACT_SELECT_STYLE}
+                                isClearable={false}
+                            />
+                        </div>
+                    </div>
+                    <div className="row margin-left10 margin-right10">
+                        <div className="input-field col s6">
+                            <input id="phone" type="text" maxLength="11" value={trafficPoliceReducer.phone} onChange={this.changePhone}/>
+                            <label htmlFor="phone">电话(登录账号)</label>
+                        </div>
+                        <div className="input-field col s6">
+                            <input id="password" type="password" maxLength="100" value={trafficPoliceReducer.password} onChange={this.changePassword}/>
+                            <label htmlFor="password">密码</label>
+                        </div>
                     </div>
 
-                    {/* 查询按钮 */}
-                    <div className="col s1">
-                        <a className="btn-floating btn-large waves-light waves-effect btn margin-top40 query-btn" onClick={this.queryPoliceList}>
-                            <i className="mdi mdi-magnify"/>
-                        </a>
+                    <div className="row margin-left10 margin-right10">
+                        <div className="input-field col s12 right-align">
+                            <button type="button" className="btn confirm-btn" onClick={updatePolice}>确定</button>
+                        </div>
                     </div>
-
-                    {/* 追加按钮 */}
-                    <div className="col s1">
-                        <a className="btn-floating btn-large waves-light waves-effect btn margin-top40 add-btn" onClick={this.queryPoliceList}>
-                            <i className="mdi mdi-plus"/>
-                        </a>
-                    </div>
-                </div>
-
-
-                {/* 下部分：检索结果显示区域 */}
-                <div className="row">
-
                 </div>
 
 
@@ -162,30 +132,52 @@ const mapStateToProps = (state) => {
 };
 
 const mapDispatchToProps = (dispatch) => ({
+    changePolicePosition: (policePosition) => {
+        dispatch(TrafficPoliceDetailActionType.setPolicePosition(policePosition))
+    },
+    changeStatus: (status) => {
+        dispatch(TrafficPoliceDetailActionType.setStatus(status))
+    },
+
+
+
+
+
+
+
+
+
+
     getPoliceList: () => {
         dispatch(trafficPoliceAction.getPoliceList())
     },
     setConditionNo: (policeNo) => {
-        dispatch(TrafficPoliceActionType.setConditionNo(policeNo))
+        dispatch(TrafficPoliceDetailActionType.setConditionNo(policeNo))
     },
     setConditionName: (name) => {
-        dispatch(TrafficPoliceActionType.setConditionName(name))
+        dispatch(TrafficPoliceDetailActionType.setConditionName(name))
     },
     changeConditionGender: (gender) => {
-        dispatch(TrafficPoliceActionType.setConditionGender(gender))
+        dispatch(TrafficPoliceDetailActionType.setConditionGender(gender))
     },
     changeConditionPosition: (policePosition) => {
-        dispatch(TrafficPoliceActionType.setConditionPosition(policePosition))
+        dispatch(TrafficPoliceDetailActionType.setConditionPosition(policePosition))
     },
     setConditionPhone: (phone) => {
-        dispatch(TrafficPoliceActionType.setConditionPhone(phone))
+        dispatch(TrafficPoliceDetailActionType.setConditionPhone(phone))
     },
     changeConditionStatus: (status) => {
-        dispatch(TrafficPoliceActionType.setConditionStatus(status))
+        dispatch(TrafficPoliceDetailActionType.setConditionStatus(status))
     },
 
 
-    addPolice: () => {
+
+
+
+
+
+
+    updatePolice: () => {
         dispatch(trafficPoliceAction.addPolice())
     }
 
