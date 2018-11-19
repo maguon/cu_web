ede03e2b6b84ffca7b8dd3245903dac8082ff9e1
yjyvsdream <jingyuan.yu@myxxjs.com>
2018-11-19 14:21:04 +0800

    修改：添加 异步上传文件 的Post 请求

--- web/src/util/HttpUtil.js
+++ web/src/util/HttpUtil.js
@@ -1,15 +1,33 @@
-const httpHeaders = require( './HttpHeaders');
+const httpHeaders = require('./HttpHeaders');
 
-const simpleFetch = (method,url,params) =>{
+export const httpAsyncFormPost = (url, formData, success, fail) => {
+    fetch('http://' + url, {
+        method :"POST",
+        body: formData
+    }).then((response) => {
+        if(response.ok){
+            return response.json();
+        }else{
+            throw new Error();
+        }
+    }).then((result) => {
+        success(result);
+    }).catch((error) => {
+        fail(error);
+    })
+};
+
+
+const simpleFetch = (method, url, params) => {
     $("#preload").show();
-    let options  ={
+    let options = {
         method: method,
         headers: httpHeaders.getHeaders()
     };
-    if('GET' != method){
+    if ('GET' != method) {
         options.body = JSON.stringify(params)
     }
-    return fetch('http://'+url, options).then((response) => {
+    return fetch('http://' + url, options).then((response) => {
         $("#preload").hide();
 
         // http 正常处理，则返回response，否则进入catch 处理
@@ -25,31 +43,31 @@ const simpleFetch = (method,url,params) =>{
     });
 };
 
-export const httpGet = (url)=> {
-    return simpleFetch('GET',url,{});
+export const httpGet = (url) => {
+    return simpleFetch('GET', url, {});
 };
 export const httpPost = (url, params) => {
-    return simpleFetch('POST',url,params)
+    return simpleFetch('POST', url, params)
 };
 export const httpPut = (url, params) => {
-    return simpleFetch('PUT',url,params)
+    return simpleFetch('PUT', url, params)
 };
 export const httpDelete = (url, params) => {
-    return simpleFetch('DELETE',url,params)
+    return simpleFetch('DELETE', url, params)
 };
 
-export const httpAsyncGet = (url,callback) => {
-    fetch('http://'+url, {
+export const httpAsyncGet = (url, callback) => {
+    fetch('http://' + url, {
         method: 'GET',
         headers: httpHeaders.headers
     }).then((response) => {
             let json = response.json()
             return json
         }
-    ).then((result)=>{
-        callback(null,result);
-    }).catch((error)=>{
-        callback(error,null);
+    ).then((result) => {
+        callback(null, result);
+    }).catch((error) => {
+        callback(error, null);
     })
 };
 
